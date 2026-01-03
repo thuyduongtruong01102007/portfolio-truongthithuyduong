5af97d44ff81d6bab28d9309a27865c6129e1519
thuyduongtruong01102007 <thuyduongtruong01102007@gmail.com>
2026-01-03 16:25:50 +0700

    Update NavLink.tsx

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -7,37 +7,37 @@ export default function Documents() {
 
       <ul style={{ lineHeight: "2" }}>
         <li>
-          <NavLink to="documents/bai-tap-01-cau-truc-thu-muc.docx">
+          <NavLink to="public/documents/bai-tap-01-cau-truc-thu-muc.docx">
             📥 Bài tập 01 – Cấu trúc thư mục
           </NavLink>
         </li>
 
         <li>
-          <NavLink to="documents/bai-tap-02-tim-kiem-thong-tin.docx">
+          <NavLink to="public/documents/bai-tap-02-tim-kiem-thong-tin.docx">
             📥 Bài tập 02 – Tìm kiếm thông tin
           </NavLink>
         </li>
 
         <li>
-          <NavLink to="documents/bai-tap-03-danh-gia-thong-tin.docx">
+          <NavLink to="public/documents/bai-tap-03-danh-gia-thong-tin.docx">
             📥 Bài tập 03 – Đánh giá thông tin
           </NavLink>
         </li>
 
         <li>
-          <NavLink to="documents/bao-cao-ai-sang-tao.docx">
+          <NavLink to="public/documents/bao-cao-ai-sang-tao.docx">
             📥 Báo cáo – AI sáng tạo
           </NavLink>
         </li>
 
         <li>
-          <NavLink to="documents/bao-cao-thuc-hanh-viet-prompt.docx">
+          <NavLink to="public/documents/bao-cao-thuc-hanh-viet-prompt.docx">
             📥 Báo cáo – Thực hành viết Prompt
           </NavLink>
         </li>
 
         <li>
-          <NavLink to="documents/su-dung-cong-cu-ho-tro.docx">
+          <NavLink to="public/documents/su-dung-cong-cu-ho-tro.docx">
             📥 Sử dụng công cụ hỗ trợ
           </NavLink>
         </li>
